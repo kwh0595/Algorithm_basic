1385b14ea764d0041dc1442081c997bd03de093a
Woohee Kim <rladngml2002@naver.com>
2024-03-11 14:44:17 +0900

    baekjoon 11726번(2xN 타일링)

--- dp/dp_11726.java
+++ dp/dp_11726.java
@@ -10,11 +10,9 @@ public class dp_11726 {
 
         int n = Integer.parseInt(br.readLine());
         int dp[] = new int[n + 1];
-        dp[0] = 0;
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i < dp.length; i++) {
-
+        for (int i = 2; i < dp.length; i++) {
             dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
         }
         System.out.println(dp[n]);
