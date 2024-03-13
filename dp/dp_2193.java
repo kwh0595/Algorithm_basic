6dbd6f0d2e72c30f66268cc7a4c08c1da9aa50cb
Woohee Kim <rladngml2002@naver.com>
2024-03-13 14:23:56 +0900

    baekjoon 2193번(이친수)

--- dp/dp_2193.java
+++ dp/dp_2193.java
@@ -9,23 +9,14 @@ public class dp_2193 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        long sum = 0;
-        int dp[][] = new int[n + 1][2];
-        dp[1][0] = 0;
-        dp[1][1] = 1;
-        for (int i = 2; i < dp.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j + 1];
-                } else {
-                    dp[i][j] = dp[i - 1][0];
-                }
-            }
-        }
-        for (int i = 0; i < 2; i++) {
-            sum += dp[n][i];
-        }
-        System.out.println(sum);
+        long dp[] = new long[n + 1];
 
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
     }
 }
